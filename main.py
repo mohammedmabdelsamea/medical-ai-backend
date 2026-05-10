6041e0194e6ce826429fc966fbc8b4b1c5127f0e
mohammedmabdelsamea <mm.abdelsamea@gmail.com>
2026-05-10 09:11:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 import requests
-import uvicorn
+import os
 
 app = FastAPI()
 
@@ -15,20 +15,24 @@ app.add_middleware(
 
 HF_API = "https://api-inference.huggingface.co/models/HumaP/vit_base_patch16_224_in21k_lung_and_colon_histopathology_pt"
 
-HF_TOKEN = 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
+HF_TOKEN = os.environ["HF_TOKEN"]
 
 @app.get("/")
-def root():
-    return {"status": "backend running"}
+def home():
+    return {"status": "running"}
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    image = await file.read()
+    try:
+        image = await file.read()
 
-    response = requests.post(
-        HF_API,
-        headers={"Authorization": f"Bearer {HF_TOKEN}"},
-        data=image
-    )
+        response = requests.post(
+            HF_API,
+            headers={"Authorization": f"Bearer {HF_TOKEN}"},
+            data=image
+        )
 
-    return response.json()
+        return response.json()
+
+    except Exception as e:
+        return {"error": str(e)}
