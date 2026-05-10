6f2be50b340524b3205f15293f989da10d58d5b6
mohammedmabdelsamea <mm.abdelsamea@gmail.com>
2026-05-10 08:36:54 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+import requests
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+HF_API = "https://api-inference.huggingface.co/models/HumaP/vit_base_patch16_224_in21k_lung_and_colon_histopathology_pt"
+
+HF_TOKEN = "PASTE_YOUR_NEW_TOKEN_HERE"
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image = await file.read()
+
+    response = requests.post(
+        HF_API,
+        headers={"Authorization": f"Bearer {HF_TOKEN}"},
+        data=image
+    )
+
+    return response.json()
