853e6fe687839eaeae7821bc76450b15450ebb07
mohammedmabdelsamea <mm.abdelsamea@gmail.com>
2026-05-10 11:34:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# 🔥 YOUR SPACE URL (replace this)
-HF_SPACE_URL = mohammedabdelsamea/medical-ai-test
+HF_SPACE_URL = https://mohammedabdelsamea-medical-ai-test.hf.space/run/predict
 
 
 @app.get("/")
@@ -23,32 +22,11 @@ def root():
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-    try:
-        image = await file.read()
+    image = await file.read()
 
-        # Spaces expect "files"
-        response = requests.post(
-            HF_SPACE_URL,
-            files={"data": image}
-        )
+    response = requests.post(
+        HF_SPACE_URL,
+        files={"data": image}
+    )
 
-        print("STATUS:", response.status_code)
-        print("RAW:", response.text)
-
-        if response.status_code != 200:
-            return {
-                "status": "error",
-                "message": "Space error",
-                "raw": response.text
-            }
-
-        return {
-            "status": "success",
-            "result": response.json()
-        }
-
-    except Exception as e:
-        return {
-            "status": "error",
-            "message": str(e)
-        }
+    return response.json()
